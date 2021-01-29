be3bfd345379e55e9a4cbf8a527f8c1d7c30c500
damian <damian@danielcastelao.org>
2021-01-29 09:52:09 +0100

    otra modificacion colaborador

--- src/com/dam1/Main.java
+++ src/com/dam1/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// comienzo a programar
         System.out.println("Primera funcionalidad terminada");
+        // colaborador nuevamente
     }
 }
